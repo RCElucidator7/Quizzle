ff7e2ef82e456a412e063f8396844a4aa39e807a
RCElucidator7 <G00332826@gmit.ie>
2018-03-27 16:24:16 +0100

    First commit, Created main page for app

---
+++ QuizGame/Settings/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace QuizGame.Settings
+{
+    public class Settings
+    {
+        public static int highScore, speed;
+
+        public static void saveSetting(string key, string contents)
+        {
+            ApplicationData.Current.LocalSettings.Values[key] = contents;
+        }
+
+        public static string loadSettings(string key)
+        {
+            var setting = ApplicationData.Current.LocalSettings;
+
+            return setting.Values.ContainsKey(key) ? setting.Values[key] as string : String.Empty;
+        }
+
+
+    }
+}
