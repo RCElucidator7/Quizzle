d212e472c902d8e22936f05a8c834c08d9932ddc
RCElucidator7 <G00332826@gmit.ie>
2018-03-27 17:44:10 +0100

    Added Second Page for user to navigate to

---
+++ QuizGame/Pages/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace QuizGame
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested -= MainPage_BackRequested;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += MainPage_BackRequested;
+
+            txtBestScore.Text = "High Score : " + Settings.Settings.loadSettings("highScore").ToString();
+        }
+
+        private async void MainPage_BackRequested(object sender, Windows.UI.Core.BackRequestedEventArgs e)
+        {
+            e.Handled = true;
+            var backMessage = new MessageDialog("Are you sure you want to close the App?");
+            var okBtn = new UICommand("Yes");
+            var cancelBtn = new UICommand("Cancel");
+            backMessage.Commands.Add(okBtn);
+            backMessage.Commands.Add(cancelBtn);
+
+            IUICommand result = await backMessage.ShowAsync();
+
+            if(result != null && result.Label.Equals("Yes"))
+            {
+                Application.Current.Exit();
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(PlayGame));
+        }
+
+        private void btnOptions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
