033e8f4c10afee04cebbc9c0532db539788006b6
RCElucidator7 <G00332826@gmit.ie>
2018-03-30 18:09:31 +0100

    Added a game over page and have buttons functioning on Play Game page

--- QuizGame/Pages/PlayGame.xaml.cs
+++ QuizGame/Pages/PlayGame.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using QuizGame.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +24,102 @@ namespace QuizGame
     /// </summary>
     public sealed partial class PlayGame : Page
     {
+        private Random rand = new Random();
+
+        private int score = 0;
+        private int highScore = 0;
+        private int state = 1;
+        private string colourResult;
+
+        private DispatcherTimer dispatcherTimer;
+
+        void setUpTimeBar()
+        {
+            //Timer set up for the timer bar in app
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Start();
+
+            timeBar.Value = 9999;
+        }
+
+        private void DispatcherTimer_Tick(object sender, object e)
+        {
+            timeBar.Value = Settings.Settings.speed;
+            if(timeBar.Value <= 0)
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Frame.Navigate(typeof(GameOver), score.ToString());
+
+            }
+        }
+
+        private int randNum()
+        {
+            return rand.Next(1, 4);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested -= PlayGame_BackRequested;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += PlayGame_BackRequested;
+
+            highScore = int.Parse(Settings.Settings.loadSettings("highScore"));
+            txtHighScore.Text = String.Format("High Score: {0}", highScore);
+            dispatcherTimer = null;
+            Playing();
+        }
+
+        private void Playing()
+        {
+            int colour = randNum();
+            if(colour == 1)
+            {
+                colourResult = "Blue";
+            }
+            else if (colour == 2)
+            {
+                colourResult = "Red";
+            }
+            else if (colour == 3)
+            {
+                colourResult = "Green";
+            }
+            else if (colour == 4)
+            {
+                colourResult = "Orange";
+            }
+
+            txtQuest.Text = String.Format("{0}", colourResult);
+
+            setUpTimeBar();
+
+        }
+
+        private async void PlayGame_BackRequested(object sender, Windows.UI.Core.BackRequestedEventArgs e)
+        {
+            e.Handled = true;
+            var backMessage = new MessageDialog("Are you sure you want to close the App?");
+            var okBtn = new UICommand("Yes");
+            var cancelBtn = new UICommand("Cancel");
+            backMessage.Commands.Add(okBtn);
+            backMessage.Commands.Add(cancelBtn);
+
+            IUICommand result = await backMessage.ShowAsync();
+
+            if (result != null && result.Label.Equals("Yes"))
+            {
+                //Navigates to the game over page and passes through score
+                Frame.Navigate(typeof(GameOver), score.ToString());
+            }
+        }
+
         public PlayGame()
         {
             this.InitializeComponent();
@@ -29,7 +127,74 @@ namespace QuizGame
 
         private void btnBlue_Click(object sender, RoutedEventArgs e)
         {
+            if("Blue" == colourResult)
+            {
+                txtScore.Text = String.Format("Score: {0}".ToUpper(), ++score);
+                txtState.Text = String.Format("{0}", ++state);
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Playing();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Frame.Navigate(typeof(GameOver), score.ToString());
+            }
+        }
 
+        private void btnRed_Click(object sender, RoutedEventArgs e)
+        {
+            if ("Red" == colourResult)
+            {
+                txtScore.Text = String.Format("Score: {0}".ToUpper(), ++score);
+                txtState.Text = String.Format("{0}", ++state);
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Playing();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Frame.Navigate(typeof(GameOver), score.ToString());
+            }
+        }
+
+        private void btnGreen_Click(object sender, RoutedEventArgs e)
+        {
+            if ("Green" == colourResult)
+            {
+                txtScore.Text = String.Format("Score: {0}".ToUpper(), ++score);
+                txtState.Text = String.Format("{0}", ++state);
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Playing();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Frame.Navigate(typeof(GameOver), score.ToString());
+            }
+        }
+
+        private void btnOrange_Click(object sender, RoutedEventArgs e)
+        {
+            if ("Orange" == colourResult)
+            {
+                txtScore.Text = String.Format("Score: {0}".ToUpper(), ++score);
+                txtState.Text = String.Format("{0}", ++state);
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Playing();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Frame.Navigate(typeof(GameOver), score.ToString());
+            }
         }
     }
 }
--- QuizGame/obj/x86/Debug/XamlTypeInfo.g.cs
+++ QuizGame/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -132,17 +132,19 @@ namespace QuizGame.QuizGame_XamlTypeInfo
 
         private void InitTypeTables()
         {
-            _typeNameTable = new string[4];
-            _typeNameTable[0] = "QuizGame.MainPage";
+            _typeNameTable = new string[5];
+            _typeNameTable[0] = "QuizGame.Pages.GameOver";
             _typeNameTable[1] = "Windows.UI.Xaml.Controls.Page";
             _typeNameTable[2] = "Windows.UI.Xaml.Controls.UserControl";
-            _typeNameTable[3] = "QuizGame.PlayGame";
+            _typeNameTable[3] = "QuizGame.MainPage";
+            _typeNameTable[4] = "QuizGame.PlayGame";
 
-            _typeTable = new global::System.Type[4];
-            _typeTable[0] = typeof(global::QuizGame.MainPage);
+            _typeTable = new global::System.Type[5];
+            _typeTable[0] = typeof(global::QuizGame.Pages.GameOver);
             _typeTable[1] = typeof(global::Windows.UI.Xaml.Controls.Page);
             _typeTable[2] = typeof(global::Windows.UI.Xaml.Controls.UserControl);
-            _typeTable[3] = typeof(global::QuizGame.PlayGame);
+            _typeTable[3] = typeof(global::QuizGame.MainPage);
+            _typeTable[4] = typeof(global::QuizGame.PlayGame);
         }
 
         private int LookupTypeIndexByName(string typeName)
@@ -177,8 +179,9 @@ namespace QuizGame.QuizGame_XamlTypeInfo
             return -1;
         }
 
-        private object Activate_0_MainPage() { return new global::QuizGame.MainPage(); }
-        private object Activate_3_PlayGame() { return new global::QuizGame.PlayGame(); }
+        private object Activate_0_GameOver() { return new global::QuizGame.Pages.GameOver(); }
+        private object Activate_3_MainPage() { return new global::QuizGame.MainPage(); }
+        private object Activate_4_PlayGame() { return new global::QuizGame.PlayGame(); }
 
         private global::Windows.UI.Xaml.Markup.IXamlType CreateXamlType(int typeIndex)
         {
@@ -190,9 +193,9 @@ namespace QuizGame.QuizGame_XamlTypeInfo
             switch (typeIndex)
             {
 
-            case 0:   //  QuizGame.MainPage
+            case 0:   //  QuizGame.Pages.GameOver
                 userType = new global::QuizGame.QuizGame_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
-                userType.Activator = Activate_0_MainPage;
+                userType.Activator = Activate_0_GameOver;
                 userType.SetIsLocalType();
                 xamlType = userType;
                 break;
@@ -205,9 +208,16 @@ namespace QuizGame.QuizGame_XamlTypeInfo
                 xamlType = new global::QuizGame.QuizGame_XamlTypeInfo.XamlSystemBaseType(typeName, type);
                 break;
 
-            case 3:   //  QuizGame.PlayGame
+            case 3:   //  QuizGame.MainPage
                 userType = new global::QuizGame.QuizGame_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
-                userType.Activator = Activate_3_PlayGame;
+                userType.Activator = Activate_3_MainPage;
+                userType.SetIsLocalType();
+                xamlType = userType;
+                break;
+
+            case 4:   //  QuizGame.PlayGame
+                userType = new global::QuizGame.QuizGame_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_4_PlayGame;
                 userType.SetIsLocalType();
                 xamlType = userType;
                 break;
